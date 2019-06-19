187a08860310402ac53dfb4b7e8708ce647221f4
Albin Jenith <albinjenith@gmail.com>
2019-06-19 22:19:33 +0530

    Initial Setup

---
+++ src/main/java/com/rovus/RovusLoginApplication.java
@@ -0,0 +1,14 @@
+package com.rovus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RovusLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RovusLoginApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/rovus/rovuslogin/RovusLoginApplicationTests.java
@@ -0,0 +1,17 @@
+package com.rovus.rovuslogin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RovusLoginApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
